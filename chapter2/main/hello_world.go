54269eb64cc3514a02131121ea8da66df7a3f56a
zxrc_suxin <suxin@innostic.com>
2022-03-19 23:24:18 +0800

    提交第二章  变量与简单类型

---
+++ chapter2/main/hello_world.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	message := "hello Go world"
+	fmt.Println(message)
+	message = "Hello Go world,Wonderfully"
+	fmt.Println(message)
+
+	name := "peter"
+	fmt.Println(strings.ToUpper(name))
+	//字符串拼接的多种方式
+
+	phone := "1300000000"
+	user := name + " " + phone
+	fmt.Println(user)
+	user1 := strings.Join([]string{"hellow", "world"}, "-")
+	fmt.Println(user1)
+	user2 := fmt.Sprintf("%s:%s", name, phone)
+	fmt.Println(user2)
+	user3 := bytes.Buffer{}
+	user3.WriteString("hello")
+	user3.WriteString(" world")
+	fmt.Println(user3.String())
+
+	//删除空格
+	foods3 := "   米饭，馒头  "
+	foods3 = strings.Trim(foods3, " ")
+	fmt.Println(foods3)
+
+	age := 25
+	fmt.Println("我的年龄是" + strconv.Itoa(age))
+}
